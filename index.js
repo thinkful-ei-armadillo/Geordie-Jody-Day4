516d97da9a8754462c0ec038ffbcb57c52dcdac8
Jodster1010 <jody.cook@mac.com>
2019-01-17 11:10:01 -0800

    fixed errors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 function handleCatClicks() {
   $('.thumbnail').on('click', function(event) {
-    const targetCat = $(event.currentTarget);
-    const otherCats = $('.thumbnail').not(targetCat);
-    const pressedBool = $(targetCat).attr('aria-pressed' === 'true');
-    otherCats.removeClass('.hero').attr('aria-pressed', false);    
-    targetCat.addClass('.hero').attr('aria-pressed', !pressedBool);
-        
+    const targetCat = $(event.currentTarget).find('img').attr('src');
+    
+    const otherCats = $(event.currentTarget).find('img').attr('alt');
+
+    $('.hero img').attr('src', targetCat).attr('alt', otherCats);  
   });
 }
 $(handleCatClicks);
+
+    // const pressedBool = $(targetCat).attr('aria-pressed' === 'true');
+    // otherCats.removeClass('.hero').attr('aria-pressed', false);    
+    // targetCat.addClass('.hero').attr('aria-pressed', !pressedBool);
